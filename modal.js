d88bb8c816ad5a2e1753b9a9e3e71936948ce8cd
gloria-tampuri <gloriatampuri15@gmail.com>
2022-04-28 14:46:19 +0000

    toggle

--- modal.js
+++ modal.js
@@ -2,19 +2,51 @@ const menuBtn = document.querySelector('#menu svg');
 const closeBtn = document.getElementById('hide');
 const backdrop = document.querySelector('.backdrop');
 const modalMenu = document.querySelector('.modal');
+const exploreDropdown = document.querySelector('.dropdowns');
+const firstDrop = document.querySelector('.dropdown');
+const plusIcon = document.querySelector('.plustoggle');
+const minusIcon =document.querySelector('.minustoggle')
+const secondDrop = document.querySelector('.drop__toggle')
 
+
+
+// funtions
 function showSideMenu (){
    
     backdrop.style.display = 'block';
     modalMenu.style.display = 'block'
 
 }
-
-menuBtn.addEventListener('click', showSideMenu)
-
 function hideModal(){
     backdrop.style.display = 'none';
     modalMenu.style.display = 'none'
+    firstDrop.style.display = 'none'
+    return hideToggle();
+    // secondDrop.style.display = 'none'
+}
+function firstToggle(){
+firstDrop.style.display = 'flex'
+plusIcon.style.display = 'block'
 }
 
+function secondToggle(){
+secondDrop.style.display = 'block'
+plusIcon.style.display = 'none';
+minusIcon.style.display = 'block'
+}
+
+function hideToggle(){
+    minusIcon.style.display = 'none';
+    plusIcon.style.display = 'block'
+    secondDrop.style.display = 'none'
+    
+}
+
+
+//  call to action;
+menuBtn.addEventListener('click', showSideMenu);
 closeBtn.addEventListener('click', hideModal);
+exploreDropdown.addEventListener('click', firstToggle);
+plusIcon.addEventListener('click', secondToggle);
+minusIcon.addEventListener('click', hideToggle)
+
