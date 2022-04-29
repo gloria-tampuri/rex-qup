5dcf6d26188c06da7f0d08b9d7e5afcee51e189f
gloria-tampuri <gloriatampuri15@gmail.com>
2022-04-29 15:44:26 +0000

    modal edit

--- modal.js
+++ modal.js
@@ -2,11 +2,16 @@ const menuBtn = document.querySelector('#menu svg');
 const closeBtn = document.getElementById('hide');
 const backdrop = document.querySelector('.backdrop');
 const modalMenu = document.querySelector('.modal');
-const exploreDropdown = document.querySelector('.dropdowns');
-const firstDrop = document.querySelector('.dropdown');
+const exploreDropdown = document.querySelector('.explore__btn');
+const exploreDropUp = document.querySelector('.explore__close');
+const firstDrop = document.querySelector('.explore__dropdown');
 const plusIcon = document.querySelector('.plustoggle');
-const minusIcon =document.querySelector('.minustoggle')
-const secondDrop = document.querySelector('.drop__toggle')
+const minusIcon =document.querySelector('.minustoggle');
+const plusIcon2 = document.querySelector('.plustoggle2');
+const minusIcon2 =document.querySelector('.minustoggle2');
+const secondDrop = document.querySelector('.drop__toggle');
+const secondDrop2 = document.querySelector('.drop__toggle2');// Added by KBQ
+const dropdownIcon = document.querySelector('.dropicon')
 
 
 
@@ -21,12 +26,25 @@ function hideModal(){
     backdrop.style.display = 'none';
     modalMenu.style.display = 'none'
     firstDrop.style.display = 'none'
+    exploreDropUp.style.display = 'none'
+    exploreDropdown.style.display = 'block'
+    hideToggle2();
     return hideToggle();
     // secondDrop.style.display = 'none'
 }
 function firstToggle(){
-firstDrop.style.display = 'flex'
-plusIcon.style.display = 'block'
+        firstDrop.style.display = 'grid'
+        minusIcon2.style.display= 'none'//Added by KBQ
+        plusIcon.style.display = 'block'
+        exploreDropUp.style.display = 'block'
+        exploreDropdown.style.display = 'none'
+}
+
+function closeToggle(){
+    exploreDropUp.style.display = 'none'
+    exploreDropdown.style.display = 'block';
+    firstDrop.style.display='none'
+       
 }
 
 function secondToggle(){
@@ -38,8 +56,20 @@ minusIcon.style.display = 'block'
 function hideToggle(){
     minusIcon.style.display = 'none';
     plusIcon.style.display = 'block'
-    secondDrop.style.display = 'none'
+    secondDrop.style.display = 'none' 
+}
+
+function secondToggle2(){
+    secondDrop2.style.display = 'block'
+    minusIcon2.style.display = 'block'//Added by KBQ
+    plusIcon2.style.display = 'none' //Added by KBQ
+    }
     
+
+function hideToggle2(){
+    minusIcon2.style.display = 'none';
+    plusIcon2.style.display = 'block'
+    secondDrop2.style.display = 'none' 
 }
 
 
@@ -49,4 +79,7 @@ closeBtn.addEventListener('click', hideModal);
 exploreDropdown.addEventListener('click', firstToggle);
 plusIcon.addEventListener('click', secondToggle);
 minusIcon.addEventListener('click', hideToggle)
+minusIcon2.addEventListener('click', hideToggle2)
+plusIcon2.addEventListener('click', secondToggle2);
+exploreDropUp.addEventListener('click', closeToggle)
 
